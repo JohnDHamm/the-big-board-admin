96fa3a9f6959d281e5e3502ab123efac6f4e7f95
John Hamm <johndhamm1@gmail.com>
2020-08-24 16:23:05 -0500

    feat(pages) add PicksPage

---
+++ src/pages/PicksPage/PicksPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { PageLayout } from '../../layout';
+
+const PicksPage: React.FC = () => {
+  return (
+    <PageLayout>
+      <div>Picks page</div>
+    </PageLayout>
+  );
+};
+
+export default PicksPage;
