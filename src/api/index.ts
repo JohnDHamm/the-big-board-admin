309d78b1e6b7ba42b1bc6b84dc8709e3f38b391e
John Hamm <johndhamm1@gmail.com>
2020-08-25 11:28:49 -0500

    feat(api): add getOverallRankings and updateOverallRanking

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,10 @@
 import { deletePlayer } from './deletePlayer/deletePlayer';
+import { getOverallRankings } from './getOverallRankings/getOverallRankings';
 import { getPlayers } from './getPlayers/getPlayers';
 import { getPositionRankings } from './getPositionRankings/getPositionRankings';
 import { getTeams } from './getTeams/getTeams';
 import { savePlayer } from './savePlayer/savePlayer';
+import { updateOverallRanking } from './updateOverallRanking/updateOverallRanking';
 import { updatePlayer } from './updatePlayer/updatePlayer';
 import { updatePositionRanking } from './updatePositionRanking/updatePositionRanking';
 
@@ -10,10 +12,12 @@ export const DEV_API_ROOT_URL = 'http://localhost:4001';
 
 export {
   deletePlayer,
+  getOverallRankings,
   getPlayers,
   getPositionRankings,
   getTeams,
   savePlayer,
+  updateOverallRanking,
   updatePlayer,
   updatePositionRanking,
 };
