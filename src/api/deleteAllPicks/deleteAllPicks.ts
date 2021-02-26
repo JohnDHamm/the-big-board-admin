1c916c896b1d1481437d9944d80c3991f0eb9a98
John Hamm <johndhamm1@gmail.com>
2021-02-26 11:35:53 -0600

    feat(api): add authentication headers to rankings and picks calls

--- src/api/deleteAllPicks/deleteAllPicks.ts
+++ src/api/deleteAllPicks/deleteAllPicks.ts
@@ -1,10 +1,12 @@
 import axios from 'axios';
 import { DEV_API_ROOT_URL } from '../index';
+import { createAuthenticationHeaders } from '../helpers';
 
 export const deleteAllPicks = async (leagueId: string) => {
   try {
     const { data } = await axios.delete(
-      `${DEV_API_ROOT_URL}/admin/remove_picks/${leagueId}`
+      `${DEV_API_ROOT_URL}/admin/remove_picks/${leagueId}`,
+      createAuthenticationHeaders()
     );
     return data;
   } catch (err) {
--- src/api/updateOverallRanking/updateOverallRanking.ts
+++ src/api/updateOverallRanking/updateOverallRanking.ts
@@ -1,15 +1,15 @@
 import axios from 'axios';
 import { DEV_API_ROOT_URL } from '../index';
+import { createAuthenticationHeaders } from '../helpers';
 
 export const updateOverallRanking = async (
   overallRank: SavedOverallRanking
 ) => {
   try {
-    const {
-      data,
-    } = await axios.patch(
+    const { data } = await axios.patch(
       `${DEV_API_ROOT_URL}/admin/overall_ranking/${overallRank._id}`,
-      { playerId: overallRank.playerId }
+      { playerId: overallRank.playerId },
+      createAuthenticationHeaders()
     );
     return data;
   } catch (err) {
